5ff2d952be1d399841983201c9563b02a7d9f563
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2020-11-04 22:34:36 -0700

    Uploading MarkDown

--- src/index.js
+++ src/index.js
@@ -1,5 +1,82 @@
 import React from 'react';
 import reactDOM from 'react-dom';
-import './styles/style.css'
-import './styles/style.scss'
+import './styles/style.css';
 
+class Editor extends React.Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        input: `# Welcome to my React Markdown Previewer!
+  
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == \'\`\`\`\' && lastLine == \'\`\`\`\') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.com), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  
+  
+  `
+      }
+      
+      this.handleChange = this.handleChange.bind(this);
+      this.someMethod = this.someMethod.bind(this);
+    };
+    
+    someMethod() {
+      
+      this.forceUpdate();
+  }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      })
+    };
+    
+  
+    render() {
+   document.getElementById('preview').innerHTML =
+        marked(this.state.input);
+      return (
+          <textarea type="text" value={this.state.input} onChange={this.handleChange.bind(this)} id='editor'> 
+        </textarea>
+      )
+    }
+  }
+  
+  ReactDOM.render(<Editor />, document.getElementById('main'))
